044d1217cf1fc6cc2dae74e215eb9a7f6301ad2e
zwh <zhongwh_00@163.com>
2020-03-01 19:41:34 +0800

    add JedisSpring

---
+++ JedisSpring/src/Test/Test.java
@@ -0,0 +1,38 @@
+import com.zwh.JedisSpring.Pojo.Person;
+import com.zwh.JedisSpring.Util.RedisTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-redis-context.xml");
+		RedisTest redisTest = (RedisTest) ctx.getBean("redisTest");
+		
+		redisTest.addStr("s1", "张三");
+		redisTest.getStr("s1");
+		
+		Person person = new Person();
+		person.setAge(18);
+		person.setName("猫猫");
+		person.setSex("0");
+		
+		redisTest.addModel("猫猫", person);
+		redisTest.getModel("猫猫");
+		
+		/*redisTest.addModel("狗狗", person);
+		redisTest.getModel("狗狗");*/
+		
+	/*	redisTest.addLink("list", "l1");
+		redisTest.addLink("list", "l2");
+		redisTest.addLink("list", "l3");
+		redisTest.getLink("list");
+		redisTest.getLink("list");
+		redisTest.getLink("list");
+		
+		redisTest.addHash("hash", "name", "张三");
+		redisTest.addHash("hash", "age", "25");
+		redisTest.getHash("hash", "name");*/
+
+	}
+	
+}
---
+++ JedisSpring/src/main/java/com/zwh/JedisSpring/Util/RedisTest.java
@@ -0,0 +1,86 @@
+package com.zwh.JedisSpring.Util;
+
+import javax.annotation.Resource;
+
+import com.zwh.JedisSpring.Pojo.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisTest {
+	
+		@Autowired
+		private StringRedisTemplate redisTemplate;
+
+		@Resource(name="redisTemplate")
+		private ValueOperations<String, Person> valueOperations;
+
+		//操作普通键值对
+		public void addStr(String key,String value){
+			redisTemplate.opsForValue().set(key, value);
+		}
+		
+	
+		public void getStr(String key){
+			System.out.println(redisTemplate.opsForValue().get(key));
+		}
+	
+		//操作链表
+		public void addLink(String key,String value){
+			redisTemplate.opsForList().leftPush(key, value);
+		}
+	
+		public void getLink(String key){
+			System.out.println(redisTemplate.opsForList().leftPop(key));
+		}
+	
+		//操作hash
+		public void addHash(String key,String hashKey,String hashValue){
+			redisTemplate.opsForHash().put(key, hashKey, hashValue);
+//		redisTemplate.opsForHash().putAll(key, map);
+		}
+	
+		public void getHash(String key,String hashKey){
+			System.err.println(redisTemplate.opsForHash().get(key, hashKey));
+		}
+	
+		//bound方法操作
+		public void boundValue(String key,String value){
+			redisTemplate.boundValueOps(key).set(value);
+			redisTemplate.boundValueOps(key).get();
+		}
+		
+		//向redis中存放实体类
+		public void addModel(String key,Person person){
+			valueOperations.set(key, person);
+		}
+		
+		public void getModel(String key){
+			System.out.println(key+":"+valueOperations.get(key));
+		}
+
+
+		public ValueOperations<String, Person> getValueOperations() {
+			return valueOperations;
+		}
+
+		public void setValueOperations(ValueOperations<String, Person> valueOperations) {
+			this.valueOperations = valueOperations;
+		}
+
+		public StringRedisTemplate getRedisTemplate() {
+			return redisTemplate;
+		}
+
+		public void setRedisTemplate(StringRedisTemplate redisTemplate) {
+			this.redisTemplate = redisTemplate;
+		}
+
+		
+		
+		
+}
